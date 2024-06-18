2405d2789bead306c67c6cfe3bcf164ebfd4c418
郑家俊 <zhengjj@hantewin.com>
2024-06-18 15:14:39 +0800

    修改同步脚本

--- 04 脚本/propertis_rsync.sh
+++ 04 脚本/propertis_rsync.sh
@@ -2,6 +2,10 @@
 user=root
 hosts='hdp02 hdp03 hdp04 hdp05 hdp06 hdp07 hdp08'
 
+echo "================     配置同步到本地              ==========="
+rsync -av ../01\ 安装部署/conf/hadoop/ $HADOOP_HOME/etc/hadoop/
+
+
 echo "================     分发环境配置文件              ==========="
 for host in $hosts
 do 
