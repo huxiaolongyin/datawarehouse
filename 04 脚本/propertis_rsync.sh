8c05450f1e86f0b631d7cba2bace2a592559aa73
郑家俊 <1453366421@qq.com>
2024-06-19 16:45:40 +0800

    添加aiui日志开发代码

--- 04 脚本/propertis_rsync.sh
+++ 04 脚本/propertis_rsync.sh
@@ -3,7 +3,7 @@ user=root
 hosts='hdp02 hdp03 hdp04 hdp05 hdp06 hdp07 hdp08'
 
 echo "================     配置同步到本地              ==========="
-rsync -av ../01\ 安装部署/conf/hadoop/ $HADOOP_HOME/etc/hadoop/
+rsync -av /root/datawarehouse/01\ 安装部署/conf/hadoop/ $HADOOP_HOME/etc/hadoop/
 
 
 echo "================     分发环境配置文件              ==========="
