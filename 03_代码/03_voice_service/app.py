009e953e33424d3b56ef4733ab02572dbd9e6023
郑家俊 <zhengjj@hantewin.com>
2024-06-26 17:49:11 +0800

    修改为本地大模型

--- 03_代码/03_voice_service/app.py
+++ 03_代码/03_voice_service/app.py
@@ -3,6 +3,9 @@ import edge_tts
 import base64
 import asyncio
 import requests
+import ollama
+
+
 app = Flask(__name__)
 
 # 接口文件
@@ -10,15 +13,15 @@ app = Flask(__name__)
 def text_to_speech():
     question_text = request.get_json()['question']
     voice= 'zh-CN-YunxiaNeural'
-    ansewer = question(question_text)
-    audio_file = asyncio.run(text_to_speech_async(ansewer, voice))
+    ansewer_text = question(question_text)
+    audio_file = asyncio.run(text_to_speech_async(ansewer_text, voice))
 
     # 回复接口内容
     response = {
         'message': 'Request received successfully',
         "voice": voice,
         'question_text': question_text,
-        'ansewer': ansewer,
+        'ansewer': ansewer_text,
         'status': 200,
         'audio_file': audio_file
     }
@@ -27,21 +30,33 @@ def text_to_speech():
 
 # 提问，通过大模型，返回内容
 def question(question_text) -> None:
-	API_URL = 'https://ms-fc-aapp-func-bjjjauogaq.cn-shanghai.fcapp.run/invoke'
-	payload = {"input":
-				{"messages":[
-					{"role":"system","content":"你是人工智能助手，名字叫笨笨同学"},
-					{"role":"user", "content":"{}".format(question_text)}
-					]},
-				"parameters":
-					{"do_sample":True,"max_length":512}}
-	with requests.Session() as session:
-		response = session.post(API_URL,json=payload)
-	return response.json()['Data']['message']['content']
+    response = ollama.chat(model='qwen2:0.5b', messages=[
+    {
+        'role': 'user',
+        'content': "{}".format(question_text),
+    },
+    {
+        'role': 'system',
+        'content': "你是人工智能助手笨笨同学"
+    },
+    ])
+    return response['message']['content']
+
+	# API_URL = 'https://ms-fc-aapp-func-bjjjauogaq.cn-shanghai.fcapp.run/invoke'
+	# payload = {"input":
+	# 			{"messages":[
+	# 				{"role":"system","content":"你是人工智能助手，名字叫笨笨同学"},
+	# 				{"role":"user", "content":"{}".format(question_text)}
+	# 				]},
+	# 			"parameters":
+	# 				{"do_sample":True,"max_length":512}}
+	# with requests.Session() as session:
+	# 	response = session.post(API_URL,json=payload)
+	# return response.json()['Data']['message']['content']
 
 async def text_to_speech_async(text, voice) -> None:
     communicate = edge_tts.Communicate(text, voice)
-    file_path = '03_代码/03_voice_service/temp/output.mp3'
+    file_path = 'D:/code/DataWarehouse/03_代码/03_voice_service/temp/output.mp3'
     await communicate.save(file_path)
     with open(file_path, 'rb') as f:
 
